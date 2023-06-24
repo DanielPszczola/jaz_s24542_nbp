f94cb48277f038b3556dab6c840d2ff2a09c9f7a
DanielPszczola <102531727+DanielPszczola@users.noreply.github.com>
2023-06-24 15:13:00 +0200

    Add files via upload

---
+++ jaz_s24542_nbp/src/main/java/com/example/jaz_s24542_nbp/JazS24542NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s24542_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS24542NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS24542NbpApplication.class, args);
+	}
+
+}
---
+++ jaz_s24542_nbp/src/test/java/com/example/jaz_s24542_nbp/JazS24542NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s24542_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS24542NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
